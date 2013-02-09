337515e2cece083e0636ee90febbd694f5a6ec83
Elliot V <elliotfriend@gmail.com>
2013-02-09 00:11:57 -0600

    added add_aliase.py. non-statically coded home directory to come

---
+++ add_alias.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+short_name = raw_input("short_name: ")
+long_name = raw_input("long_name: ")
+
+with open("home/elliot/.bash_aliases", "a") as open_file:
+    open_file.write("alias %s='%s'\n" % ( short_name, long_name ))
+
+open_file.close()
