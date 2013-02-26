916d7fb2835551917f1e1b192cfb1cb4287e20c6
Elliot V <elliotfriend@gmail.com>
2013-02-25 21:45:10 -0600

    put open_file.close() in an "if" statement, probly don't need it most of the time

--- add_alias.py
+++ add_alias.py
@@ -6,4 +6,6 @@ long_name = raw_input("long_name: ")
 with open("/home/elliot/.bash_aliases", "a") as open_file:
     open_file.write("alias %s='%s'\n" % ( short_name, long_name ))
 
-open_file.close()
+# Sanity check
+if open_file.closed == False:
+    open_file.close()
