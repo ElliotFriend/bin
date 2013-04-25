af350d4c2f01feefe588b352b92dec8616665e40
Elliot V <elliotfriend@gmail.com>
2013-04-24 20:06:43 -0500

    added graceful exit on keyboard interrupt

--- add_alias.py
+++ add_alias.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
+
 import sys
 
-if sys.version_info[0] == 2:
-    short_name = raw_input("short_name: ")
-    long_name = raw_input("long_name: ")
-elif sys.version_info[0] == 3:
-    short_name = input("short_name: ")
-    long_name = input("long_name: ")
+try:
+    if sys.version_info[0] == 2:
+        short_name = raw_input("short_name: ")
+        long_name = raw_input("long_name: ")
+    elif sys.version_info[0] == 3:
+        short_name = input("short_name: ")
+        long_name = input("long_name: ")
 
-with open("/home/elliot/.bash_aliases", "a") as open_file:
-    open_file.write("alias %s='%s'\n" % ( short_name, long_name ))
+    with open("/home/elliot/.bash_aliases", "a") as open_file:
+        open_file.write("alias %s='%s'\n" % ( short_name, long_name ))
 
-# Sanity check
-if open_file.closed == False:
-    open_file.close()
+    # Sanity check
+    if open_file.closed == False:
+        open_file.close()
+except KeyboardInterrupt:
+    sys.exit()
