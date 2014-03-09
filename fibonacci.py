24607144135a33fcae4c319ac9886b2192d30718
Elliot Voris <elliotfriend@gmail.com>
2014-03-09 00:51:00 -0600

    We don't really need to save the ending number in a variable, when it's just as easy to put it into the range.
    Signed-off-by: Elliot Voris <elliotfriend@gmail.com>

--- fibonacci.py
+++ fibonacci.py
@@ -3,9 +3,8 @@
 import sys
 
 f_seq = [ 0, 1 ]
-f_end = int(sys.argv[1]) - 1
 
-for num in range(f_seq[0], f_end):
+for num in range(f_seq[0], int(sys.argv[1]) - 1):
     f_seq.append(f_seq[-1] + f_seq[-2])
 
 print f_seq
