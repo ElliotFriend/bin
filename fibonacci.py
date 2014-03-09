fb2480dd8d864729bfd603a16a780c8b5aa3792c
Elliot Voris <elliotfriend@gmail.com>
2014-03-09 00:37:00 -0600

    Added .gitignore. Also, if you ever wanted to find Fn in the Fibonacci sequence, run 'fibonacci.py n'
    Signed-off-by: Elliot Voris <elliotfriend@gmail.com>

---
+++ fibonacci.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import sys
+
+f_seq = [ 0, 1 ]
+f_end = int(sys.argv[1]) - 1
+
+for num in range(f_seq[0], f_end):
+    f_seq.append(f_seq[-1] + f_seq[-2])
+
+print f_seq
+print "length is ", len(f_seq)
