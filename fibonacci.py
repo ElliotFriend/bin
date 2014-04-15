01dedafeac8b97b4800b9ab2759b02b0dd7a707a
Elliot V <elliotfriend@gmail.com>
2014-04-15 00:33:52 -0500

    It makes much more sense to loop through the code dependent on the length of the list. After all, we're trying to get to a list that is n digits long. The for loop worked, but it didn't quite capture the purpose of generating a fibonacci sequence.
    Signed-off-by: Elliot V <elliotfriend@gmail.com>

--- fibonacci.py
+++ fibonacci.py
@@ -5,10 +5,9 @@ import sys
 # Start the sequence. It always starts with [0, 1]
 f_seq = [ 0, 1 ]
 
-# We're doing a one extra digit here; not everybody
-# will want the sequence to start at 0, and may want
-# to start it with [ 1, 1 ]
-for num in range(f_seq[0], int(sys.argv[1]) - 1):
+# Until the length of our list reaches the limit that
+# the user has specified, continue to add to the sequence
+while len(f_seq) <= int(sys.argv[1]):
     # Add the last two numbers in the list, and stick
     # that onto the end of the list
     f_seq.append(f_seq[-1] + f_seq[-2])
